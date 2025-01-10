bfa18a111540a6e2be1e20f9c45a0dd6e52c1361
ibrohimjon1209 <ibrohimjonmamirov120909@gmail.com>
2025-01-10 22:45:34 +0500

    something

--- src/Dashboard_status/Status.jsx
+++ src/Dashboard_status/Status.jsx
@@ -1,128 +1,51 @@
-import React from 'react'
-import student_icon from './Image/students_icon.png'
-import lid_icon from './Image/lid_icon.png'
-import debtor_icon from './Image/debtor_icon.png'
-import freeze_icon from './Image/freeze_icon.png'
-import graduate_icon from './Image/graduate_icon.png'
-import money_icon from './Image/money_icon.png'
-import archive_icon from './Image/archive_icon.png'
-import icon from './Image/icon.png'
-
+import React from 'react';
+import student_icon from './Image/students_icon.png';
+import lid_icon from './Image/lid_icon.png';
+import debtor_icon from './Image/debtor_icon.png';
+import freeze_icon from './Image/freeze_icon.png';
+import graduate_icon from './Image/graduate_icon.png';
+import money_icon from './Image/money_icon.png';
+import archive_icon from './Image/archive_icon.png';
+import icon from './Image/icon.png';
 
 const Status = () => {
-    return (
-        <div className='px-[32px] pt-[33px] flex flex-col gap-[15px] mb-[26px]'>
+  const data = [
+    { label: 'Buyurtma', value: 8, icon: icon },
+    { label: 'Birinchi daraga keladiganlar', value: 18, icon: icon },
+    { label: 'Yangi o\'quvchi', value: 38, icon: icon },
+    { label: 'Aktiv o\'quvchilar', value: 135, icon: icon },
+    { label: 'Buyurtmadan ketganlar', value: 0, icon: icon },
+    { label: 'Yangi o\'quvchidan ketganlar', value: 0, icon: icon },
+    { label: 'Aktiv o\'quychidan ketganlar', value: 0, icon: icon },
+    { label: 'Qarzdor', value: 45, icon: icon },
+    { label: 'Guruhlar', value: 16, icon: icon },
+    { label: 'Birinchi to\'lovni qilganlar', value: 2, icon: icon },
+    { label: 'Muzlatilgan', value: 0, icon: icon },
+    { label: 'Arxiv', value: 0, icon: icon },
+  ];
 
-            <div className="first flex gap-[20px]">
-
-
-                <div className='w-[239px] h-[80px] pl-[27px] flex items-center gap-[35px] rounded-[10px] bg-[rgba(255,255,255,1)] shadow-lg shadow-[rgba(13,153,255,0.25)]'>
-                    <img src={icon} className='w-[55px] h-[55px] object-cover' />
-                    <div className='flex flex-col gap-[5px] justify-center'>
-                        <h1 className='font-[roboto] font-[400] text-[16px] leading-[18.75px] text-[rgba(0,0,0,0.5)]'>Buyurtma</h1>
-                        <h1 className='font-[alata] font-[400] text-[14px] leading-[19.32px] text-[rgba(0,0,0,1)]'>8</h1>
-                    </div>
-                </div>
-
-                <div className='w-[239px] h-[80px] pl-[27px] flex items-center gap-[35px] rounded-[10px] bg-[rgba(255,255,255,1)] shadow-lg shadow-[rgba(13,153,255,0.25)]'>
-                    <img src={icon} className='w-[55px] h-[55px] object-cover' />
-                    <div className='flex flex-col gap-[5px] justify-center'>
-                        <h1 className='font-[roboto] font-[400] text-[16px] leading-[18.75px] text-[rgba(0,0,0,0.5)]'>Buyurtma</h1>
-                        <h1 className='font-[alata] font-[400] text-[14px] leading-[19.32px] text-[rgba(0,0,0,1)]'>8</h1>
-                    </div>
-                </div>
-
-                <div className='w-[239px] h-[80px] pl-[27px] flex items-center gap-[35px] rounded-[10px] bg-[rgba(255,255,255,1)] shadow-lg shadow-[rgba(13,153,255,0.25)]'>
-                    <img src={icon} className='w-[55px] h-[55px] object-cover' />
-                    <div className='flex flex-col gap-[5px] justify-center'>
-                        <h1 className='font-[roboto] font-[400] text-[16px] leading-[18.75px] text-[rgba(0,0,0,0.5)]'>Buyurtma</h1>
-                        <h1 className='font-[alata] font-[400] text-[14px] leading-[19.32px] text-[rgba(0,0,0,1)]'>8</h1>
-                    </div>
-                </div>
-
-                <div className='w-[239px] h-[80px] pl-[27px] flex items-center gap-[35px] rounded-[10px] bg-[rgba(255,255,255,1)] shadow-lg shadow-[rgba(13,153,255,0.25)]'>
-                    <img src={icon} className='w-[55px] h-[55px] object-cover' />
-                    <div className='flex flex-col gap-[5px] justify-center'>
-                        <h1 className='font-[roboto] font-[400] text-[16px] leading-[18.75px] text-[rgba(0,0,0,0.5)]'>Buyurtma</h1>
-                        <h1 className='font-[alata] font-[400] text-[14px] leading-[19.32px] text-[rgba(0,0,0,1)]'>8</h1>
-                    </div>
-                </div>
-
-                <div className='w-[239px] h-[80px] pl-[27px] flex items-center gap-[35px] rounded-[10px] bg-[rgba(255,255,255,1)] shadow-lg shadow-[rgba(13,153,255,0.25)]'>
-                    <img src={icon} className='w-[55px] h-[55px] object-cover' />
-                    <div className='flex flex-col gap-[5px] justify-center'>
-                        <h1 className='font-[roboto] font-[400] text-[16px] leading-[18.75px] text-[rgba(0,0,0,0.5)]'>Buyurtma</h1>
-                        <h1 className='font-[alata] font-[400] text-[14px] leading-[19.32px] text-[rgba(0,0,0,1)]'>8</h1>
-                    </div>
-                </div>
-
-                <div className='w-[239px] h-[80px] pl-[27px] flex items-center gap-[35px] rounded-[10px] bg-[rgba(255,255,255,1)] shadow-lg shadow-[rgba(13,153,255,0.25)]'>
-                    <img src={icon} className='w-[55px] h-[55px] object-cover' />
-                    <div className='flex flex-col gap-[5px] justify-center'>
-                        <h1 className='font-[roboto] font-[400] text-[16px] leading-[18.75px] text-[rgba(0,0,0,0.5)]'>Buyurtma</h1>
-                        <h1 className='font-[alata] font-[400] text-[14px] leading-[19.32px] text-[rgba(0,0,0,1)]'>8</h1>
-                    </div>
-                </div>
-
+  return (
+    <div className="px-[32px] pt-[33px] flex flex-col gap-[15px] mb-[26px]">
+      <div className="first flex flex-wrap gap-y-[20px] gap-x-[1.7%]">
+        {data.map((item, index) => (
+          <div
+            key={index}
+            className="w-[15.2%] h-[80px] pl-[27px] flex items-center gap-[10%] rounded-[10px] bg-[rgba(255,255,255,1)] shadow-lg shadow-[rgba(13,153,255,0.25)]"
+          >
+            <img src={item.icon} className="w-[55px] h-[55px] object-cover" />
+            <div className="flex flex-col gap-[5px] justify-center">
+              <h1 className="font-[roboto] font-[400] text-[13px] leading-[18.75px] text-[rgba(0,0,0,0.5)]">
+                {item.label}
+              </h1>
+              <h1 className="font-[alata] font-[400] text-[14px] leading-[19.32px] text-[rgba(0,0,0,1)]">
+                {item.value}
+              </h1>
             </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-
-            <div className="second flex gap-[20px]">
-
-
-                <div className='w-[239px] h-[80px] pl-[27px] flex items-center gap-[35px] rounded-[10px] bg-[rgba(255,255,255,1)] shadow-lg shadow-[rgba(13,153,255,0.25)]'>
-                    <img src={icon} className='w-[55px] h-[55px] object-cover' />
-                    <div className='flex flex-col gap-[5px] justify-center'>
-                        <h1 className='font-[roboto] font-[400] text-[16px] leading-[18.75px] text-[rgba(0,0,0,0.5)]'>Buyurtma</h1>
-                        <h1 className='font-[alata] font-[400] text-[14px] leading-[19.32px] text-[rgba(0,0,0,1)]'>8</h1>
-                    </div>
-                </div>
-
-                <div className='w-[239px] h-[80px] pl-[27px] flex items-center gap-[35px] rounded-[10px] bg-[rgba(255,255,255,1)] shadow-lg shadow-[rgba(13,153,255,0.25)]'>
-                    <img src={icon} className='w-[55px] h-[55px] object-cover' />
-                    <div className='flex flex-col gap-[5px] justify-center'>
-                        <h1 className='font-[roboto] font-[400] text-[16px] leading-[18.75px] text-[rgba(0,0,0,0.5)]'>Buyurtma</h1>
-                        <h1 className='font-[alata] font-[400] text-[14px] leading-[19.32px] text-[rgba(0,0,0,1)]'>8</h1>
-                    </div>
-                </div>
-
-                <div className='w-[239px] h-[80px] pl-[27px] flex items-center gap-[35px] rounded-[10px] bg-[rgba(255,255,255,1)] shadow-lg shadow-[rgba(13,153,255,0.25)]'>
-                    <img src={icon} className='w-[55px] h-[55px] object-cover' />
-                    <div className='flex flex-col gap-[5px] justify-center'>
-                        <h1 className='font-[roboto] font-[400] text-[16px] leading-[18.75px] text-[rgba(0,0,0,0.5)]'>Buyurtma</h1>
-                        <h1 className='font-[alata] font-[400] text-[14px] leading-[19.32px] text-[rgba(0,0,0,1)]'>8</h1>
-                    </div>
-                </div>
-
-                <div className='w-[239px] h-[80px] pl-[27px] flex items-center gap-[35px] rounded-[10px] bg-[rgba(255,255,255,1)] shadow-lg shadow-[rgba(13,153,255,0.25)]'>
-                    <img src={icon} className='w-[55px] h-[55px] object-cover' />
-                    <div className='flex flex-col gap-[5px] justify-center'>
-                        <h1 className='font-[roboto] font-[400] text-[16px] leading-[18.75px] text-[rgba(0,0,0,0.5)]'>Buyurtma</h1>
-                        <h1 className='font-[alata] font-[400] text-[14px] leading-[19.32px] text-[rgba(0,0,0,1)]'>8</h1>
-                    </div>
-                </div>
-
-                <div className='w-[239px] h-[80px] pl-[27px] flex items-center gap-[35px] rounded-[10px] bg-[rgba(255,255,255,1)] shadow-lg shadow-[rgba(13,153,255,0.25)]'>
-                    <img src={icon} className='w-[55px] h-[55px] object-cover' />
-                    <div className='flex flex-col gap-[5px] justify-center'>
-                        <h1 className='font-[roboto] font-[400] text-[16px] leading-[18.75px] text-[rgba(0,0,0,0.5)]'>Buyurtma</h1>
-                        <h1 className='font-[alata] font-[400] text-[14px] leading-[19.32px] text-[rgba(0,0,0,1)]'>8</h1>
-                    </div>
-                </div>
-
-                <div className='w-[239px] h-[80px] pl-[27px] flex items-center gap-[35px] rounded-[10px] bg-[rgba(255,255,255,1)] shadow-lg shadow-[rgba(13,153,255,0.25)]'>
-                    <img src={icon} className='w-[55px] h-[55px] object-cover' />
-                    <div className='flex flex-col gap-[5px] justify-center'>
-                        <h1 className='font-[roboto] font-[400] text-[16px] leading-[18.75px] text-[rgba(0,0,0,0.5)]'>Buyurtma</h1>
-                        <h1 className='font-[alata] font-[400] text-[14px] leading-[19.32px] text-[rgba(0,0,0,1)]'>8</h1>
-                    </div>
-                </div>
-
-            </div>
-
-
-        </div>
-    )
-}
-
-export default Status
+export default Status;
--- src/Table/Test.jsx
+++ src/Table/Test.jsx
@@ -1,51 +1,314 @@
-import React from 'react';
+'use client'
+import { useState, useEffect } from 'react'
+import { motion, AnimatePresence } from "framer-motion"
+import classNames from 'classnames';
 
-function Test() {
-    const timeSlots = Array(20).fill("08:00 - 08:30");
-    const zones = Array(8).fill(null).map((_, i) => `${i + 1}-xona`);
+const Test = () => {
+    const customScrollbar = {
+        overflowY: 'auto', // Enable vertical scrolling
+        scrollbarWidth: 'auto', // For Firefox
+        scrollbarColor: 'rgba(0,53,95,1) rgba(0, 0, 95, 1)', // For Firefox
+    };
+
+    // WebKit scrollbar styles
+    const webkitScrollbarStyles = {
+        width: '100px', // Width of the scrollbar
+    };
+
+    const webkitScrollbarTrackStyles = {
+        background: 'rgba(0,53,95,1)', // Background of the scrollbar track
+        borderRadius: '100px', // Rounded corners for track
+    };
+
+    const webkitScrollbarThumbStyles = {
+        background: 'rgb(51, 0, 255)', // Color of the scrollbar thumb
+        borderRadius: '100px', // Rounded corners for thumb
+    };
+
+
+
+
+    const days = ["Yak", "Du", "Se", "Chor", "Pa", "Ju", "Sha"];
+    const rooms = ["1-xona", "2-xona", "3-xona", "4-xona", "5-xona", "6-xona", "7-xona", "8-xona", "9-xona"];
+    const timeSlots = [
+        "08:00 - 08:30", "08:30 - 09:00", "09:00 - 09:30", "09:30 - 10:30",
+        "10:30 - 11:00", "11:00 - 11:30", "11:30 - 12:00", "12:00 - 12:30",
+        "12:30 - 13:00", "13:00 - 13:30", "13:30 - 14:00", "14:00 - 14:30", "14:30 - 15:00"
+    ];
+
+    const getCurrentDay = () => {
+        const today = new Date();
+        const dayIndex = today.getDay(); // 0: Yak, 1: Du, ..., 6: Sha
+        return days[dayIndex];
+    };
+
+    const [activeDay, setActiveDay] = useState(getCurrentDay());
+
+    const classesByDay = {
+        Du: [
+            {
+                room: "5-xona",
+                startTime: "10:30 - 11:00",
+                name: "Toq kun Ertalab A1",
+                teacher: "Oydin Yusupova",
+                progress: "29/53",
+                status: "O'tildi",
+                color: "bg-emerald-500",
+                rowSpan: 1
+            },
+        ],
+
+        Se: [
+            {
+                room: "5-xona",
+                startTime: "10:30 - 11:00",
+                name: "Toq kun Ertalab A1",
+                teacher: "Oydin Yusupova",
+                progress: "29/53",
+                status: "O'tildi",
+                color: "bg-emerald-500",
+                rowSpan: 1
+            },
+
+            {
+                room: "2-xona",
+                startTime: "10:30 - 11:00",
+                name: "Toq kun Ertalab A1",
+                teacher: "Oydin Yusupova",
+                progress: "29/53",
+                status: "O'tildi",
+                color: "bg-emerald-500",
+                rowSpan: 1
+            },
+
+            {
+                room: "8-xona",
+                startTime: "10:30 - 11:00",
+                name: "Toq kun Ertalab A1",
+                teacher: "Oydin Yusupova",
+                progress: "29/53",
+                status: "O'tildi",
+                color: "bg-emerald-500",
+                rowSpan: 2
+            },
+        ],
+
+        Chor: [
+            {
+                room: "5-xona",
+                startTime: "10:30 - 11:00",
+                name: "Toq kun Ertalab A1",
+                teacher: "Oydin Yusupova",
+                progress: "29/53",
+                status: "O'tildi",
+                color: "bg-emerald-500",
+                rowSpan: 1
+            },
+            {
+                room: "5-xona",
+                startTime: "10:30 - 11:00",
+                name: "Toq kun Ertalab A1",
+                teacher: "Oydin Yusupova",
+                progress: "29/53",
+                status: "O'tildi",
+                color: "bg-emerald-500",
+                rowSpan: 3
+            },
+        ],
+
+        Pa: [
+
+            {
+                room: "5-xona",
+                startTime: "10:30 - 11:00",
+                name: "Juft kun Ertalab B1",
+                teacher: "Oydin Yusupova",
+                progress: "15/30",
+                status: "O'tildi",
+                color: "bg-emerald-500",
+                rowSpan: 4
+            },
+            {
+                room: "3-xona",
+                startTime: "11:30 - 12:00",
+                name: "IELTS Advanced",
+                teacher: "John Smith",
+                progress: "20/25",
+                status: "O'tildi",
+                color: "bg-blue-500",
+                rowSpan: 3
+            }
+        ],
+
+        Ju: [
+
+            {
+                room: "3-xona",
+                startTime: "8:00 - 8:30",
+                name: "Juft kun Ertalab B1",
+                teacher: "Oydin Yusupova",
+                progress: "15/30",
+                status: "O'tildi",
+                color: "bg-emerald-500",
+                rowSpan: 4
+            },
+            {
+                room: "3-xona",
+                startTime: "11:30 - 12:00",
+                name: "IELTS Advanced",
+                teacher: "John Smith",
+                progress: "20/25",
+                status: "O'tildi",
+                color: "bg-blue-500",
+                rowSpan: 3
+            }
+        ],
+
+        Sha: [
+
+            {
+                room: "5-xona",
+                startTime: "10:30 - 11:00",
+                name: "Juft kun Ertalab B1",
+                teacher: "Oydin Yusupova",
+                progress: "15/30",
+                status: "O'tildi",
+                color: "bg-emerald-500",
+                rowSpan: 4
+            },
+            {
+                room: "3-xona",
+                startTime: "11:30 - 12:00",
+                name: "IELTS Advanced",
+                teacher: "John Smith",
+                progress: "20/25",
+                status: "O'tildi",
+                color: "bg-blue-500",
+                rowSpan: 3
+            }
+        ],
+
+
+        Yak: [
+
+            {
+                room: "5-xona",
+                startTime: "10:30 - 11:00",
+                name: "Juft kun Ertalab B1",
+                teacher: "Oydin Yusupova",
+                progress: "15/30",
+                status: "O'tildi",
+                color: "bg-emerald-500",
+                rowSpan: 4
+            },
+            {
+                room: "3-xona",
+                startTime: "11:30 - 12:00",
+                name: "IELTS Advanced",
+                teacher: "John Smith",
+                progress: "20/25",
+                status: "O'tildi",
+                color: "bg-blue-500",
+                rowSpan: 3
+            }
+        ],
+    };
 
     return (
-        <div className='ml-[40px] w-[95%] h-[600px] overflow-y-auto overflow-x-auto'>
+        <div className='pt-[0px] px-[32px] '>
+            <div className="w-full h-full shadow-lg shadow-[rgba(0,0,0,0.25)] rounded-[15px] pt-[15px] px-[18px] bg-[rgba(255,255,255,1)] overflow-x-auto">
+                {/* Days header */}
+                <div className="flex gap-[14px] mb-3">
+                    {days.map((day) => (
+                        <button
+                            key={day}
+                            onClick={() => setActiveDay(day)}
+                            className={classNames(
+                                "w-[70px] h-[30px] flex justify-center items-center text-sm rounded-full transition-all duration-300 transform hover:scale-110",
+                                day === activeDay
+                                    ? "bg-[rgba(38,78,134,1)] text-white shadow-lg"
+                                    : "bg-[rgba(242,238,238,1)] hover:bg-[#e6e4e4]"
+                            )}
+                        >
+                            {day}
+                        </button>
+                    ))}
+                </div>
 
-            <div className="w-full h-full overflow-x-auto overflow-y-auto ml-[0px] border-[1px] border-[rgba(74,106,201,1)] rounded-r-[15px]">
-                <table className="border-[1px] border-[rgba(203,204,205,1)] w-full min-w-[800px]">
-                    <thead>
-                        <tr>
-                            <div className="flex w-[2105px] bg-white flex-col items-center justify-center">
-                                <th className="relative left-[20px] text-[rgba(64,64,64,1)]">Xona</th>
-                                <div className="w-20 h-[1px] rotate-[20deg] bg-black"></div>
-                                <th className="relative left-[-20px] text-[rgba(64,64,64,1)]">Vaqt</th>
+                {/* Table */}
+                <div className="table w-full overflow-hidden border-[2px] border-[rgba(161,161,161,1)] bg-white shadow-lg rounded-[12px]">
+
+                    <div
+                        className='flex-1'
+                        style={{ ...customScrollbar, overflowY: 'auto', scrollbarWidth: 'thin', scrollbarColor: 'rgba(0,53,95,1) rgba(255, 255, 255, 1)', }}>
+                        <div style={webkitScrollbarStyles} className="scrollbar">
+                            <div style={webkitScrollbarTrackStyles} className="scrollbar-track">
+                                <div style={webkitScrollbarThumbStyles} className="scrollbar-thumb"></div>
                             </div>
-                            {zones.map((zone) => (
-                                <th
-                                    key={zone}
-                                    className="border-[1px] w-[2150px] border-[rgba(203,204,205,1)] p-2 bg-white text-center text-black"
-                                >
-                                    <h1>{zone}</h1>
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {timeSlots.map((time, i) => (
-                            <tr key={i}>
-                                <td className="w-[2000px] bg-white border-[1px] border-[rgba(203,204,205,1)] p-2">{time}</td>
-                                {zones.map((_, j) => (
-                                    <td key={j} className="border-[1px] bg-white border-[rgba(203,204,205,1)] p-2">
-                                        {/* <div className="bg-blue-500 text-white p-2 rounded-md m-1">
-                            <div className="my-1">Guruh : IT</div>
-                            <div className="my-1">Ustozi : Falonchi</div>
-                            <div className="my-1">O'quchilar soni : 35/40</div>
-                        </div> */}
-                                    </td>
+                        </div>
+
+
+                        <div className=''>
+                            {/* Room numbers header */}
+                            <div className={`grid grid-cols-10 border-b border-[rgba(161,161,161,1)]`}>
+                                {console.log(rooms.length)}
+                                <div className="w-[148px] border-r border-[rgba(161,161,161,1)]"></div>
+                                {rooms.map((room) => (
+                                    <div key={room} className="h-[44px] flex justify-center items-center border-r border-[rgba(161,161,161,1)]">
+                                        <h1 className='font-[roboto] font-[400] text-[14px] leading-[16.41px] text-[rgba(0,0,0,1)]'>{room}</h1>
+                                    </div>
                                 ))}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                            </div>
+
+                            {/* Time slots */}
+                            {timeSlots.map((time) => (
+                                <div key={time} className={`grid grid-cols-${10} border-b border-[rgba(161,161,161,1)]`}>
+                                    <div className="w-[148px] h-[45px] flex justify-center items-center border-r border-[rgba(161,161,161,1)] text-sm text-center">
+                                        <h1 className='font-[roboto] font-[100] text-[12px] leading-[14.06px] text-[rgba(0,0,0,0.5)]'>
+                                            {time}
+                                        </h1>
+                                    </div>
+                                    {rooms.map((room) => {
+                                        const classItem = classesByDay[activeDay]?.find(
+                                            c => c.room === room && c.startTime === time
+                                        );
+
+                                        return (
+                                            <div key={`${room}-${time}`} className="border-r border-[rgba(161,161,161,1)] h-[45px] relative">
+                                                <AnimatePresence>
+                                                    {classItem && (
+                                                        <motion.div
+                                                            initial={{ opacity: 0, scale: 0.9 }}
+                                                            animate={{ opacity: 1, scale: 1 }}
+                                                            exit={{ opacity: 0, scale: 0.9 }}
+                                                            transition={{ duration: 0.2 }}
+                                                            className={`${classItem.color} text-white p-[5px] rounded-md text-sm absolute w-auto inset-1`}
+                                                            style={{
+                                                                height: classItem.rowSpan ? `${classItem.rowSpan * 45 - 7}px` : 'auto',
+                                                            }}
+                                                        >
+                                                            <div>{classItem.name}</div>
+                                                            <div>{classItem.teacher}</div>
+                                                            <div>{classItem.progress}</div>
+                                                            <div>{classItem.status}</div>
+                                                        </motion.div>
+                                                    )}
+                                                </AnimatePresence>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            ))}
+                        </div>
+
+
+                    </div>
+
+
+                </div>
             </div>
         </div>
     );
-}
+};
 
 export default Test;
