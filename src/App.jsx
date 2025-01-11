4cef545d87ca83504d1870da0048eb6c81e57b6a
the_yodgorbek01 <yodgorbekziyoitdinov@gmail.com>
2025-01-11 11:31:28 +0500

    news

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Nav_top from './pages/Navbar/Nav-top';
 import Main_home from './pages/home/main_home';
 import Not_Found from './pages/not_foun/not_found';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { div } from 'framer-motion/client';
 
 const App = () => {
   const customScrollbar = {
@@ -12,50 +13,44 @@ const App = () => {
     scrollbarColor: 'rgba(0,53,95,1) rgba(0, 0, 95, 1)',
   };
 
-  const webkitScrollbarStyles = {
-    width: '100px',
-  };
-
-  const webkitScrollbarTrackStyles = {
-    background: 'rgba(0,53,95,1)',
-    borderRadius: '100px',
-  };
-
-  const webkitScrollbarThumbStyles = {
-    background: 'rgb(51, 0, 255)',
-    borderRadius: '100px',
-  };
-
   return (
-    <div className='flex w-full  bg-[rgba(242,238,238,1)] m-auto h-screen'>
-      <div className='h-screen sticky top-0'>
-        <Navbar />
+    <Router>
+      <div className='flex w-full bg-[rgba(242,238,238,1)] m-auto h-screen'>
+        <Routes>
+          <Route
+            path="*"
+            element={
+              <div className='flex w-[100vw] justify-center bg-white'>
+                <Not_Found />
+              </div>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <div className='flex w-full h-screen'>
+                <div className='h-screen sticky top-0'>
+                  <Navbar />
+                </div>
+                <div className='flex flex-col w-full overflow-hidden'>
+                  <Nav_top />
+                  <div
+                    className='flex-1'
+                    style={{
+                      ...customScrollbar,
+                      overflowY: 'auto',
+                      scrollbarWidth: 'thin',
+                      scrollbarColor: 'rgba(0,53,95,1) rgba(255, 255, 255, 1)',
+                    }}>
+                    <Main_home />
+                  </div>
+                </div>
+              </div>
+            }
+          />
+        </Routes>
       </div>
-
-      <div className='flex flex-col w-full overflow-hidden'>
-        <Nav_top />
-        <div
-          className='flex-1'
-          style={{
-            ...customScrollbar,
-            overflowY: 'auto',
-            scrollbarWidth: 'thin',
-            scrollbarColor: 'rgba(0,53,95,1) rgba(255, 255, 255, 1)',
-          }}>
-          <div style={webkitScrollbarStyles} className="scrollbar">
-            <div style={webkitScrollbarTrackStyles} className="scrollbar-track">
-              <div style={webkitScrollbarThumbStyles} className="scrollbar-thumb"></div>
-            </div>
-          </div>
-          <Router>
-            <Routes>
-              <Route path="*" element={<Not_Found />} />
-              <Route path="/" element={<Main_home />} />
-            </Routes>
-          </Router>
-        </div>
-      </div>
-    </div>
+    </Router>
   );
 };
 
---
+++ src/pages/home/main_home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Status from "../Dashboard_status/Status";
+import Table from "../Table/main";
+
+const Main_home = () => {
+    return <>
+        <Status />
+        <Table />
+    </>;
+};
+
+export default Main_home;
