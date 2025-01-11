f89f2e4be98802e2506a0f8ecdd696b4e9d18972
ibrohimjon1209 <ibrohimjonmamirov120909@gmail.com>
2025-01-11 14:45:18 +0500

    adfasdfasdf

--- src/pages/Navbar/Nav-top.jsx
+++ src/pages/Navbar/Nav-top.jsx
@@ -5,6 +5,7 @@ import search from './Image/black/search.png';
 import question_icon from './Image/blue/question mark.png';
 import message_have_icon from './Image/blue/message_have.png';
 import notification_have_icon from './Image/blue/notification_have.png';
+import { Link } from 'react-router-dom';
 
 const Nav_top = () => {
   const [selectedOption, setSelectedOption] = useState('NOON');
@@ -17,7 +18,10 @@ const Nav_top = () => {
     <div className="w-full h-[97px] flex px-[40px] justify-between shadow-[0px_2px_2px_0_#00000040] items-center bg-[rgba(255,255,255,1)] sticky top-0">
       <div className="flex gap-[25px]">
         <div className='flex items-center justify-center gap-[13px] cursor-pointer'>
-          <img src={back} alt="" className='w-[24px] h-[24px]'/>
+          <Link to='/'>
+            <img src={back} alt="" className='w-[24px] h-[24px]' />
+
+          </Link>
           <p className='font-[roboto] font-[400] text-[18px] leading-[21.09px] text-black'>Orqaga</p>
         </div>
         <div className="cursor-pointer centerzones flex justify-center items-center w-[163px] h-[49px] rounded-[12px] border-[2px] border-[#0000004D] relative ml-[28px]">
@@ -25,16 +29,16 @@ const Nav_top = () => {
             value={selectedOption}
             onChange={(e) => handleOptionChange(e.target.value)}
             className="cursor-pointer font-[roboto] font-[400] text-[18px] text-center leading-[21.09px] text-[rgba(0,0,0,1)] bg-transparent outline-none appearance-none w-full h-full"
-            >
+          >
             <option className='cursor-pointer' value="NOON">NOON</option>
             <option className='cursor-pointer' value="Option 2">Option 2</option>
             <option className='cursor-pointer' value="Option 3">Option 3</option>
           </select>
         </div>
-          <div className="relative flex items-center justify-center ml-[30px]">
-            <input type="text" placeholder="Qidirish ..." className='w-[173px] h-[49px] pl-[10px] pr-[35px] text-black rounded-[12px] border-[2px] border-[#0000004D] bg-[rgba(255,255,255,1)]' />
-            <img src={search} className="w-[20px] h-[20px] absolute right-[10px] object-contain cursor-pointer"/>
-          </div>
+        <div className="relative flex items-center justify-center ml-[30px]">
+          <input type="text" placeholder="Qidirish ..." className='w-[173px] h-[49px] pl-[10px] pr-[35px] text-black rounded-[12px] border-[2px] border-[#0000004D] bg-[rgba(255,255,255,1)]' />
+          <img src={search} className="w-[20px] h-[20px] absolute right-[10px] object-contain cursor-pointer" />
+        </div>
       </div>
 
       <div className="flex gap-[20px]">
@@ -42,11 +46,11 @@ const Nav_top = () => {
           <p className='font-[400] text-[16px] leading-[22.08px] text-[#0EA5E9] cursor-pointer' style={{ fontFamily: 'Alata' }}>O'zb</p>
           <p className='font-[400] text-[16px] leading-[22.08px] text-black cursor-pointer' style={{ fontFamily: 'Alata' }}>Рус</p>
           <p className='font-[400] text-[16px] leading-[22.08px] text-black cursor-pointer' style={{ fontFamily: 'Alata' }}>Eng</p>
-          <img src={question_icon} className='w-[33px] h-[33px] cursor-pointer'/>
-          <img src={message_have_icon} className='w-[33px] h-[33px] cursor-pointer'/>
-          <img src={notification_have_icon} className='w-[33px] h-[33px] cursor-pointer'/>
+          <img src={question_icon} className='w-[33px] h-[33px] cursor-pointer' />
+          <img src={message_have_icon} className='w-[33px] h-[33px] cursor-pointer' />
+          <img src={notification_have_icon} className='w-[33px] h-[33px] cursor-pointer' />
         </div>
-        <img src={profile} className="w-[39px] h-[39px] rounded-[50%] cursor-pointer"/>
+        <img src={profile} className="w-[39px] h-[39px] rounded-[50%] cursor-pointer" />
       </div>
     </div>
   );
--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import { useState, React } from 'react';
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import assignment_icon from './Image/blue/assignment.png';
 import lydes_icon from './Image/blue/lydes.png';
 import group_icon from './Image/blue/group.png';
@@ -11,112 +12,229 @@ import marketing_icon from './Image/blue/marketing.png';
 import report_icon from './Image/blue/report.png';
 import settings_icon from './Image/blue/settings.png';
 import logo from './Image/black/logo vector.png';
-import './style.css'
+import './style.css';
 
 const Navbar = () => {
+  const [isStudentMenuOpen, setStudentMenuOpen] = useState(false);
+  const location = useLocation();
 
+  return (
+    <div className="w-[320px] top-0 h-full flex flex-col justify-between bg-white">
+      <div className="w-[100%] py-[15px] flex justify-center items-center shadow-[0.2px_2px_2px_0_#00000040]">
+        <Link to="/">
+          <img
+            src={logo}
+            alt="logo"
+            className="w-[67px] h-[67px] object-contain cursor-pointer"
+          />
+        </Link>
+      </div>
+      <div className="duration-300 nav-left w-[100%] h-full mt-[28px] gap-[5px] overflow-y-hidden overflow-x-hidden flex flex-col items-center">
+        <Link
+          to="/assigments"
+          className={`w-[85%] rounded-[10px] flex items-center cursor-pointer transition-all duration-300 ${
+            location.pathname === '/assigments'
+              ? 'bg-[#CFEBFF] text-[#264E86]'
+              : 'bg-white text-black hover:bg-[#CFEBFF] hover:text-[#264E86]'
+          } pl-[25px]`}
+        >
+          <div className="w-[101px] h-[60px] flex gap-[30px] items-center">
+            <img
+              src={assignment_icon}
+              className="w-[38px] h-[38px] object-contain"
+            />
+            <h1 className="font-[roboto] font-[400] text-[18px] leading-[21.09px] whitespace-nowrap">
+              Topshiriqlar
+            </h1>
+          </div>
+        </Link>
 
-    const [isStudentMenuOpen, setStudentMenuOpen] = useState(false);
+        <Link
+          to="/lids"
+          className={`w-[85%] rounded-[10px] flex items-center cursor-pointer transition-all duration-300 pl-[25px] ${
+            location.pathname === '/lids'
+              ? 'bg-[#CFEBFF] text-[#264E86]'
+              : 'bg-white text-black hover:bg-[#CFEBFF] hover:text-[#264E86]'
+          }`}
+        >
+          <div className="w-[101px] h-[60px] flex gap-[30px] items-center">
+            <img src={lydes_icon} className="w-[38px] h-[38px] object-contain" />
+            <h1 className="font-[roboto] font-[400] text-[18px] leading-[21.09px] whitespace-nowrap">
+              Lidlar
+            </h1>
+          </div>
+        </Link>
 
-    return (
-        <div className=' w-[320px] top-0 h-full flex flex-col justify-between bg-white'>
-            <div className='w-[100%] py-[15px] flex justify-center items-center shadow-[0.2px_2px_2px_0_#00000040]'>
-                <img src={logo} alt="logo" className='w-[67px] h-[67px] object-contain cursor-pointer' />
-            </div>
-            <div className='duration-300 nav-left w-[100%] flex h-full mt-[28px] flex-col gap-[5px] overflow-y-hidden overflow-x-hidden flex flex-col items-center'>
-                <div className='w-[85%] rounded-[10px] flex items-center cursor-pointer bg-[#CFEBFF] pl-[25px]'>
-                    <div className='w-[101px] h-[60px] flex gap-[30px] items-center'>
-                        <img src={assignment_icon} className='w-[38px] h-[38px] object-contain'/>
-                        <h1 className='font-[roboto] font-[400] text-[18px] leading-[21.09px] text-[#264E86] whitespace-nowrap'>Topshiriqlar</h1>
-                    </div>
-                </div>
+        <Link
+          to="/groups"
+          className={`w-[85%] rounded-[10px] flex items-center cursor-pointer transition-all duration-300 pl-[25px] ${
+            location.pathname === '/groups'
+              ? 'bg-[#CFEBFF] text-[#264E86]'
+              : 'bg-white text-black hover:bg-[#CFEBFF] hover:text-[#264E86]'
+          }`}
+        >
+          <div className="w-[101px] h-[60px] flex gap-[30px] items-center">
+            <img src={group_icon} className="w-[38px] h-[38px] object-contain" />
+            <h1 className="font-[roboto] font-[400] text-[18px] leading-[21.09px] whitespace-nowrap">
+              Guruh
+            </h1>
+          </div>
+        </Link>
 
-                <div className={`${isStudentMenuOpen
-                    ? 'w-[227.29px] h-[63.26px] rounded-[8px] flex items-center flex-col bg-[rgba(245,245,245,1)]'
-                    : 'w-[85%] rounded-[10px] flex items-center cursor-pointer bg-white pl-[25px]'
-                    }`}
-                >
-                    <div onClick={setStudentMenuOpen} className={`${isStudentMenuOpen
-                        ? 'w-[101px] h-[60px] flex gap-[30px] items-center'
-                        : 'w-[101px] h-[60px] flex gap-[30px] items-center'
-                        }`}>
-                        <img src={lydes_icon} className='w-[38px] h-[38px] object-contain'/>
-                        <h1 className='font-[roboto] font-[400] text-[18px] leading-[21.09px] text-black whitespace-nowrap'>Lidlar</h1>
-                    </div>
-                    {isStudentMenuOpen && (
-                        <div className='duration-200 w-full h-[129px] bg-[rgba(245,245,245,1)] rounded-b-[8px]'>
-                            <div className='text-black'>• Ro’yxat</div>
-                            <div className='text-black'>• Davomat</div>
-                        </div>
-                    )}
-                </div>
+        <Link
+          to="/students"
+          className={`w-[85%] rounded-[10px] flex items-center cursor-pointer transition-all duration-300 pl-[25px] ${
+            location.pathname === '/students'
+              ? 'bg-[#CFEBFF] text-[#264E86]'
+              : 'bg-white text-black hover:bg-[#CFEBFF] hover:text-[#264E86]'
+          }`}
+        >
+          <div className="w-[101px] h-[60px] flex gap-[30px] items-center">
+            <img
+              src={student_icon}
+              className="w-[38px] h-[38px] object-contain"
+            />
+            <h1 className="font-[roboto] font-[400] text-[18px] leading-[21.09px] whitespace-nowrap">
+              O'quvchilar
+            </h1>
+          </div>
+        </Link>
 
+        <Link
+          to="/study_section"
+          className={`w-[85%] rounded-[10px] flex items-center cursor-pointer transition-all duration-300 pl-[25px] ${
+            location.pathname === '/study_section'
+              ? 'bg-[#CFEBFF] text-[#264E86]'
+              : 'bg-white text-black hover:bg-[#CFEBFF] hover:text-[#264E86]'
+          }`}
+        >
+          <div className="w-[101px] h-[60px] flex gap-[30px] items-center">
+            <img
+              src={study_division}
+              className="w-[38px] h-[38px] object-contain"
+            />
+            <h1 className="font-[roboto] font-[400] text-[18px] leading-[21.09px] whitespace-nowrap">
+              O'quv bo'limi
+            </h1>
+          </div>
+        </Link>
 
-                <div className='w-[85%] rounded-[10px] flex items-center cursor-pointer pl-[25px]'>
-                    <div className='w-[101px] h-[60px] flex gap-[30px] items-center'>
-                        <img src={group_icon} className='w-[38px] h-[38px] object-contain'/>
-                        <h1 className='font-[roboto] font-[400] text-[18px] leading-[21.09px] text-black whitespace-nowrap'>Guruh</h1>
-                    </div>
-                </div>
+        <Link
+          to="/finance"
+          className={`w-[85%] rounded-[10px] flex items-center cursor-pointer transition-all duration-300 pl-[25px] ${
+            location.pathname === '/finance'
+              ? 'bg-[#CFEBFF] text-[#264E86]'
+              : 'bg-white text-black hover:bg-[#CFEBFF] hover:text-[#264E86]'
+          }`}
+        >
+          <div className="w-[101px] h-[60px] flex gap-[30px] items-center">
+            <img
+              src={wallet_icon}
+              className="w-[38px] h-[38px] object-contain"
+            />
+            <h1 className="font-[roboto] font-[400] text-[18px] leading-[21.09px] whitespace-nowrap">
+              Moliya
+            </h1>
+          </div>
+        </Link>
 
-                <div className='w-[85%] rounded-[10px] flex items-center cursor-pointer pl-[25px]'>
-                    <div className='w-[101px] h-[60px] flex gap-[30px] items-center'>
-                        <img src={student_icon} className='w-[38px] h-[38px] object-contain'/>
-                        <h1 className='font-[roboto] font-[400] text-[18px] leading-[21.09px] text-black whitespace-nowrap'>O’quvchilar</h1>
-                    </div>
-                </div>
+        <Link
+          to="/control"
+          className={`w-[85%] rounded-[10px] flex items-center cursor-pointer transition-all duration-300 pl-[25px] ${
+            location.pathname === '/control'
+              ? 'bg-[#CFEBFF] text-[#264E86]'
+              : 'bg-white text-black hover:bg-[#CFEBFF] hover:text-[#264E86]'
+          }`}
+        >
+          <div className="w-[101px] h-[60px] flex gap-[30px] items-center">
+            <img
+              src={control_icon}
+              className="w-[38px] h-[38px] object-contain"
+            />
+            <h1 className="font-[roboto] font-[400] text-[18px] leading-[21.09px] whitespace-nowrap">
+              Nazorat
+            </h1>
+          </div>
+        </Link>
 
-                <div className='w-[85%] rounded-[10px] flex items-center cursor-pointer pl-[25px]'>
-                    <div className='w-[101px] h-[60px] flex gap-[30px] items-center'>
-                        <img src={study_division} className='w-[38px] h-[38px] object-contain'/>
-                        <h1 className='font-[roboto] font-[400] text-[18px] leading-[21.09px] text-black whitespace-nowrap'>O’quv bo’limi</h1>
-                    </div>
-                </div>
+        <Link
+          to="/management"
+          className={`w-[85%] rounded-[10px] flex items-center cursor-pointer transition-all duration-300 pl-[25px] ${
+            location.pathname === '/management'
+              ? 'bg-[#CFEBFF] text-[#264E86]'
+              : 'bg-white text-black hover:bg-[#CFEBFF] hover:text-[#264E86]'
+          }`}
+        >
+          <div className="w-[101px] h-[60px] flex gap-[30px] items-center">
+            <img
+              src={drive_icon}
+              className="w-[38px] h-[38px] object-contain"
+            />
+            <h1 className="font-[roboto] font-[400] text-[18px] leading-[21.09px] whitespace-nowrap">
+              Boshqaruv
+            </h1>
+          </div>
+        </Link>
 
-                <div className='w-[85%] rounded-[10px] flex items-center cursor-pointer pl-[25px]'>
-                    <div className='w-[101px] h-[60px] flex gap-[30px] items-center'>
-                        <img src={wallet_icon} className='w-[38px] h-[38px] object-contain'/>
-                        <h1 className='font-[roboto] font-[400] text-[18px] leading-[21.09px] text-black whitespace-nowrap'>Moliya</h1>
-                    </div>
-                </div>
+        <Link
+          to="/marketing"
+          className={`w-[85%] rounded-[10px] flex items-center cursor-pointer transition-all duration-300 pl-[25px] ${
+            location.pathname === '/marketing'
+              ? 'bg-[#CFEBFF] text-[#264E86]'
+              : 'bg-white text-black hover:bg-[#CFEBFF] hover:text-[#264E86]'
+          }`}
+        >
+          <div className="w-[101px] h-[60px] flex gap-[30px] items-center">
+            <img
+              src={marketing_icon}
+              className="w-[38px] h-[38px] object-contain"
+            />
+            <h1 className="font-[roboto] font-[400] text-[18px] leading-[21.09px] whitespace-nowrap">
+              Sotuv va marketing
+            </h1>
+          </div>
+        </Link>
 
-                <div className='w-[85%] rounded-[10px] flex items-center cursor-pointer pl-[25px]'>
-                    <div className='w-[101px] h-[60px] flex gap-[30px] items-center'>
-                        <img src={control_icon} className='w-[38px] h-[38px] object-contain'/>
-                        <h1 className='font-[roboto] font-[400] text-[18px] leading-[21.09px] text-black whitespace-nowrap'>Nazorat</h1>
-                    </div>
-                </div>
+        <Link
+          to="/reports"
+          className={`w-[85%] rounded-[10px] flex items-center cursor-pointer transition-all duration-300 pl-[25px] ${
+            location.pathname === '/reports'
+              ? 'bg-[#CFEBFF] text-[#264E86]'
+              : 'bg-white text-black hover:bg-[#CFEBFF] hover:text-[#264E86]'
+          }`}
+        >
+          <div className="w-[101px] h-[60px] flex gap-[30px] items-center">
+            <img
+              src={report_icon}
+              className="w-[38px] h-[38px] object-contain"
+            />
+            <h1 className="font-[roboto] font-[400] text-[18px] leading-[21.09px] whitespace-nowrap">
+              Hisobotlar
+            </h1>
+          </div>
+        </Link>
 
-                <div className='w-[85%] rounded-[10px] flex items-center cursor-pointer pl-[25px]'>
-                    <div className='w-[101px] h-[60px] flex gap-[30px] items-center'>
-                        <img src={drive_icon} className='w-[38px] h-[38px] object-contain'/>
-                        <h1 className='font-[roboto] font-[400] text-[18px] leading-[21.09px] text-black whitespace-nowrap'>Boshqaruv</h1>
-                    </div>
-                </div>
-
-                <div className='w-[85%] rounded-[10px] flex items-center cursor-pointer pl-[25px]'>
-                    <div className='w-[101px] h-[60px] flex gap-[30px] items-center'>
-                        <img src={marketing_icon} className='w-[38px] h-[38px] object-contain'/>
-                        <h1 className='font-[roboto] font-[400] text-[18px] leading-[21.09px] text-black whitespace-nowrap'>Sotuv va marketing</h1>
-                    </div>
-                </div>
-
-                <div className='w-[85%] rounded-[10px] flex items-center cursor-pointer pl-[25px]'>
-                    <div className='w-[101px] h-[60px] flex gap-[30px] items-center'>
-                        <img src={report_icon} className='w-[38px] h-[38px] object-contain'/>
-                        <h1 className='font-[roboto] font-[400] text-[18px] leading-[21.09px] text-black whitespace-nowrap'>Hisobotlar</h1>
-                    </div>
-                </div>
-
-                <div className='w-[85%] rounded-[10px] flex items-center cursor-pointer pl-[25px]'>
-                    <div className='w-[101px] h-[60px] flex gap-[30px] items-center'>
-                        <img src={settings_icon} className='w-[38px] h-[38px] object-contain'/>
-                        <h1 className='font-[roboto] font-[400] text-[18px] leading-[21.09px] text-black whitespace-nowrap'>Sozlamalar</h1>
-                    </div>
-                </div>
-            </div>
-        </div >
-    );
-}
+        <Link
+          to="/settings"
+          className={`w-[85%] rounded-[10px] flex items-center cursor-pointer transition-all duration-300 pl-[25px] ${
+            location.pathname === '/settings'
+              ? 'bg-[#CFEBFF] text-[#264E86]'
+              : 'bg-white text-black hover:bg-[#CFEBFF] hover:text-[#264E86]'
+          }`}
+        >
+          <div className="w-[101px] h-[60px] flex gap-[30px] items-center">
+            <img
+              src={settings_icon}
+              className="w-[38px] h-[38px] object-contain"
+            />
+            <h1 className="font-[roboto] font-[400] text-[18px] leading-[21.09px] whitespace-nowrap">
+              Sozlamalar
+            </h1>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
