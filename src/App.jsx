3e5b19da658abe8001f1620b958d8b41eea63a82
the_yodgorbek01 <yodgorbekziyoitdinov@gmail.com>
2025-01-11 12:28:34 +0500

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import Navbar from './pages/Navbar/Navbar';
 import Nav_top from './pages/Navbar/Nav-top';
 import Main_home from './pages/home/main_home';
-import Not_Found from './pages/404_page/not_found';
+import Assigments from './pages/assigments/assigments';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { div } from 'framer-motion/client';
 
 const App = () => {
   const customScrollbar = {
@@ -16,39 +15,26 @@ const App = () => {
   return (
     <Router>
       <div className='flex w-full bg-[rgba(242,238,238,1)] m-auto h-screen'>
-        <Routes>
-          <Route
-            path="*"
-            element={
-              <div className='flex w-[100vw] justify-center bg-white'>
-                <Not_Found />
-              </div>
-            }
-          />
-          <Route
-            path="/"
-            element={
-              <div className='flex w-full h-screen'>
-                <div className='h-screen sticky top-0'>
-                  <Navbar />
-                </div>
-                <div className='flex flex-col w-full overflow-hidden'>
-                  <Nav_top />
-                  <div
-                    className='flex-1'
-                    style={{
-                      ...customScrollbar,
-                      overflowY: 'auto',
-                      scrollbarWidth: 'thin',
-                      scrollbarColor: 'rgba(0,53,95,1) rgba(255, 255, 255, 1)',
-                    }}>
-                    <Main_home />
-                  </div>
-                </div>
-              </div>
-            }
-          />
-        </Routes>
+        <div className='h-screen sticky top-0'>
+          <Navbar />
+        </div>
+        <div className='flex flex-col w-full overflow-hidden'>
+          <Nav_top />
+          <div
+            className='flex-1'
+            style={{
+              ...customScrollbar,
+              overflowY: 'auto',
+              scrollbarWidth: 'thin',
+              scrollbarColor: 'rgba(0,53,95,1) rgba(255, 255, 255, 1)',
+            }}
+          >
+            <Routes>
+              <Route path="/" element={<Main_home />} />
+              <Route path="/assigments" element={<Assigments />} />
+            </Routes>
+          </div>
+        </div>
       </div>
     </Router>
   );
---
+++ src/pages/assigments/assigments.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main_home = () => {
+    return <div>main_home</div>;
+};
+
+export default Main_home;
