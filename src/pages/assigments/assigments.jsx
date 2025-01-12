1c56333690edfcb204f106bfff9091059edcf5cf
the_yodgorbek01 <yodgorbekziyoitdinov@gmail.com>
2025-01-12 17:57:49 +0500

    asd

--- src/pages/assigments/assigments.jsx
+++ src/pages/assigments/assigments.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import red_phone_icon from "./images/red_phone_icon.png";
 import green_phone_icon from "./images/green_phone_icon.png";
 import filter_icon from "./images/filter_icon.png";
 import arrow_down_icon from "./images/arrow_down_icon.png";
+import arrow_down_black_icon from "./images/arrow_down_black_icon.png";
+import calendar_icon from "./images/calendar_icon.png";
 import add_icon from "./images/add_icon.png";
 import './style.css'
 
@@ -316,21 +318,63 @@ const feature_count = () => {
 }
 
 const Assigments = () => {
+
+    const [is_filter_open, set_is_filter_open] = useState(false);
+
     return (
     <>
     <div className="w-full h-auto flex flex-col items-center mt-[15px]">
         <div className="w-[100%] px-[2%] flex flex-row justify-between items-baseline">
             <h1 className="font-[roboto] font-[100] text-[20px] leading-[23.44px] text-[#264E86]">Topshiriqlar</h1>
             <div className="flex flex-row gap-[10px]">
-            <div className="w-[177px] h-[55px] bg-[#0D99FF] rounded-[5px] flex items-center justify-between px-[15px] cursor-pointer">
+            <div 
+                className="w-[177px] h-[55px] bg-[#0D99FF] rounded-[5px] flex items-center justify-between px-[15px] cursor-pointer"
+                onClick={
+                    () => {
+                        set_is_filter_open(!is_filter_open);
+                    }
+                }
+            >
                 <img src={filter_icon} alt="" className="w-[31px] h-[31px] object-contain"/>
                 <p className="font-[roboto] font-[600] text-[20px] leading-[23.44px] text-white">Filtr</p>
-                <img src={arrow_down_icon} alt="" />
+                <img src={arrow_down_icon} className={is_filter_open ? "w-[31px] h-[31px] object-contain rotate-180" : "w-[31px] h-[31px] object-contain"} alt="" />
             </div>
             <div className="w-[177px] h-[55px] bg-[#0D99FF] rounded-[5px] flex items-center justify-start pl-[15px] gap-[20px] cursor-pointer">
                 <img src={add_icon} alt="" />
                 <p className="font-[roboto] font-[600] text-[20px] leading-[23.44px] text-white">Qo’shish</p>
             </div>
+            </div>
+        </div>
+        <div className={is_filter_open ? "flex flex-row gap-[10px] pl-[130px] pt-[30px]" : "flex flex-row gap-[10px] pl-[130px] pt-[30px] hidden"}>
+            <div className="w-[240px] h-[55px] bg-white rounded-[5px] flex items-center justify-between px-[28px]">
+                <p className="font-roboto font-[500] text-[19px] leading-[23.44px] text-black">
+                    Mas'ul shaxs
+                </p>
+                <img src={arrow_down_black_icon} className="w-[31px] h-[31px] object-contain cursor-pointer" alt="" />
+            </div>
+            <div className="w-[240px] h-[55px] bg-white rounded-[5px] flex items-center justify-between px-[28px]">
+                <p className="font-roboto font-[500] text-[19px] leading-[23.44px] text-black">
+                    O’quvchi
+                </p>
+                <img src={arrow_down_black_icon} className="w-[31px] h-[31px] object-contain cursor-pointer" alt="" />
+            </div>
+            <div className="w-[240px] h-[55px] bg-white rounded-[5px] flex items-center justify-between px-[28px]">
+                <p className="font-roboto font-[500] text-[19px] leading-[23.44px] text-black">
+                    Topshiriq turi
+                </p>
+                <img src={arrow_down_black_icon} className="w-[31px] h-[31px] object-contain cursor-pointer" alt="" />
+            </div>
+            <div className="w-[240px] h-[55px] bg-white rounded-[5px] flex items-center justify-between px-[25px]">
+                <img src={calendar_icon} className="w-[31px] h-[31px] object-contain cursor-pointer" alt="" />
+                <p className="font-roboto font-[500] text-[19px] leading-[23.44px] text-black">
+                    Oraliqni tanlang
+                </p>
+            </div>
+            <div className="w-[240px] h-[55px] bg-white rounded-[5px] flex items-center justify-between px-[28px]">
+                <p className="font-roboto font-[500] text-[19px] leading-[23.44px] text-black">
+                    Guruh
+                </p>
+                <img src={arrow_down_black_icon} className="w-[31px] h-[31px] object-contain cursor-pointer" alt="" />
             </div>
         </div>
     <div className="w-auto h-[94%] flex flex-col ml-[32px] mt-[20px] mr-[323px] gap-[25px] overflow-y-hidden">
