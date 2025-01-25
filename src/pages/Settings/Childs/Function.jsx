8e8806a3ff377a30ba0ed3ae5df051f992d40e89
ibrohimjon1209 <ibrohimjonmamirov120909@gmail.com>
2025-01-25 21:12:28 +0500

    asderer

--- src/pages/Settings/Childs/Function.jsx
+++ src/pages/Settings/Childs/Function.jsx
@@ -1,148 +1,151 @@
 import React from 'react';
 import on from './../Images/Vector.png'
+import { div } from 'framer-motion/client';
 
 const Function = () => {
     return (
-        <div className="w-[812px] h-[80vh] overflow-hidden pt-[15px] rounded-[20px] bg-[rgba(255,255,255,1)]">
-            <div className="w-full h-full overflow-y-scroll">
-                <div className='w-full h-[3506px] pt-[10px] px-[40px]'>
-                    <h1 className="font-[roboto] font-[600] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]">
-                        Funksionallik
-                    </h1>
+        <div className='pb-[45px]'>
+            <div className="w-[812px] h-[calc(100vh-180px)] overflow-hidden pt-[15px] rounded-[20px] bg-[rgba(255,255,255,1)]">
+                <div className="w-full h-full overflow-y-scroll">
+                    <div className='w-full h-[3506px] pt-[10px] px-[40px]'>
+                        <h1 className="font-[roboto] font-[600] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]">
+                            Funksionallik
+                        </h1>
 
-                    <div className='mt-[10px]'>
-                        <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
-                            <div className='w-[677px]'>
-                                <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Band xona</h1>
+                        <div className='mt-[10px]'>
+                            <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
+                                <div className='w-[677px]'>
+                                    <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Band xona</h1>
+                                </div>
+                                <img src={on} alt="" />
                             </div>
-                            <img src={on} alt="" />
+
+                            <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
+                                <div className='w-[677px]'>
+                                    <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Davomat funksionalligi</h1>
+                                </div>
+                                <img src={on} alt="" />
+                            </div>
+
+                            <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
+                                <div className='w-[677px]'>
+                                    <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Davomatdan oldin topshiriq yaratish</h1>
+                                </div>
+                                <img src={on} alt="" />
+                            </div>
+
+                            <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
+                                <div className='w-[677px]'>
+                                    <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Topshiriq yaratishdan oldin baholash</h1>
+                                </div>
+                                <img src={on} alt="" />
+                            </div>
+
+                            <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
+                                <div className='w-[677px]'>
+                                    <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Uchta darsga kelmaganlarni avtomatik guruhdan chiqarib yuborish va o'quvchi adminiga avtotask qo'shilib qolishi</h1>
+                                </div>
+                                <img src={on} alt="" />
+                            </div>
+
+                            <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
+                                <div className='w-[677px]'>
+                                    <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Moliya bo'limida o'qituvchi bo'yicha filtr</h1>
+                                </div>
+                                <img src={on} alt="" />
+                            </div>
+
+                            <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
+                                <div className='w-[677px]'>
+                                    <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Maktab uchun guruh</h1>
+                                </div>
+                                <img src={on} alt="" />
+                            </div>
+
+                            <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
+                                <div className='w-[677px]'>
+                                    <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Kassalar orasida tasdiqlash orqali ko'chirish</h1>
+                                </div>
+                                <img src={on} alt="" />
+                            </div>
+
+                            <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
+                                <div className='w-[677px]'>
+                                    <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Support funksiyasini yoqish </h1>
+                                </div>
+                                <img src={on} alt="" />
+                            </div>
+
+                            <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
+                                <div className='w-[677px]'>
+                                    <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Botga davomat qilinmadi</h1>
+                                </div>
+                                <img src={on} alt="" />
+                            </div>
+
+                            <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
+                                <div className='w-[677px]'>
+                                    <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Yordamchi o'qituvchini bron qilish blokini yoqish</h1>
+                                </div>
+                                <img src={on} alt="" />
+                            </div>
+
+                            <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
+                                <div className='w-[677px]'>
+                                    <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Yordamchi o'qituvchini bron qilish blokini yoqish</h1>
+                                </div>
+                                <img src={on} alt="" />
+                            </div>
+
+
+                            <div className='w-full h-[148px] gap-[20px] border-b-[1.5px] flex flex-col px-[10px]'>
+                                <div className='w-[677px] mt-[20px]'>
+                                    <h1 className='text-[18px] font-[500] font-[inter] text-[rgba(64,64,64,1)]'>Davomatda hamma keldi turishi</h1>
+                                </div>
+
+                                <div className='w-[248px] h-[67px] bg-[rgba(242,238,238,1)] '>
+
+                                </div>
+                            </div>
+
+
+
+                            <div className='w-full h-[148px] gap-[20px] border-b-[1.5px] flex flex-col px-[10px]'>
+                                <div className='w-[677px] mt-[20px]'>
+                                    <h1 className='text-[18px] font-[500] font-[inter] text-[rgba(64,64,64,1)]'>O'qituvchining ish haqi ko'rsatkichi</h1>
+                                </div>
+
+                                <div className='w-[248px] h-[67px] bg-[rgba(242,238,238,1)] '>
+
+                                </div>
+                            </div>
+
+                            <div className='w-full h-[148px] gap-[20px] border-b-[1.5px] flex flex-col px-[10px]'>
+                                <div className='w-[677px] mt-[20px]'>
+                                    <h1 className='text-[18px] font-[500] font-[inter] text-[rgba(64,64,64,1)]'>Qarzdorlik limiti</h1>
+                                </div>
+
+                                <div className='w-[248px] h-[67px] bg-[rgba(242,238,238,1)] '>
+
+                                </div>
+                            </div>
+
+
+
+
+
+
+
+
+
+                            <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
+                                <div className='w-[677px]'>
+                                    <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Band xona</h1>
+                                </div>
+                                <img src={on} alt="" />
+                            </div>
+
                         </div>
-
-                        <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
-                            <div className='w-[677px]'>
-                                <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Davomat funksionalligi</h1>
-                            </div>
-                            <img src={on} alt="" />
-                        </div>
-
-                        <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
-                            <div className='w-[677px]'>
-                                <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Davomatdan oldin topshiriq yaratish</h1>
-                            </div>
-                            <img src={on} alt="" />
-                        </div>
-
-                        <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
-                            <div className='w-[677px]'>
-                                <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Topshiriq yaratishdan oldin baholash</h1>
-                            </div>
-                            <img src={on} alt="" />
-                        </div>
-
-                        <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
-                            <div className='w-[677px]'>
-                                <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Uchta darsga kelmaganlarni avtomatik guruhdan chiqarib yuborish va o'quvchi adminiga avtotask qo'shilib qolishi</h1>
-                            </div>
-                            <img src={on} alt="" />
-                        </div>
-
-                        <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
-                            <div className='w-[677px]'>
-                                <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Moliya bo'limida o'qituvchi bo'yicha filtr</h1>
-                            </div>
-                            <img src={on} alt="" />
-                        </div>
-
-                        <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
-                            <div className='w-[677px]'>
-                                <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Maktab uchun guruh</h1>
-                            </div>
-                            <img src={on} alt="" />
-                        </div>
-
-                        <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
-                            <div className='w-[677px]'>
-                                <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Kassalar orasida tasdiqlash orqali ko'chirish</h1>
-                            </div>
-                            <img src={on} alt="" />
-                        </div>
-
-                        <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
-                            <div className='w-[677px]'>
-                                <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Support funksiyasini yoqish </h1>
-                            </div>
-                            <img src={on} alt="" />
-                        </div>
-
-                        <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
-                            <div className='w-[677px]'>
-                                <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Botga davomat qilinmadi</h1>
-                            </div>
-                            <img src={on} alt="" />
-                        </div>
-
-                        <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
-                            <div className='w-[677px]'>
-                                <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Yordamchi o'qituvchini bron qilish blokini yoqish</h1>
-                            </div>
-                            <img src={on} alt="" />
-                        </div>
-
-                        <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
-                            <div className='w-[677px]'>
-                                <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Yordamchi o'qituvchini bron qilish blokini yoqish</h1>
-                            </div>
-                            <img src={on} alt="" />
-                        </div>
-
-
-                        <div className='w-full h-[148px] gap-[20px] border-b-[1.5px] flex flex-col px-[10px]'>
-                            <div className='w-[677px] mt-[20px]'>
-                                <h1 className='text-[18px] font-[500] font-[inter] text-[rgba(64,64,64,1)]'>Davomatda hamma keldi turishi</h1>
-                            </div>
-
-                            <div className='w-[248px] h-[67px] bg-[rgba(242,238,238,1)] '>
-
-                            </div>
-                        </div>
-
-
-
-                        <div className='w-full h-[148px] gap-[20px] border-b-[1.5px] flex flex-col px-[10px]'>
-                            <div className='w-[677px] mt-[20px]'>
-                                <h1 className='text-[18px] font-[500] font-[inter] text-[rgba(64,64,64,1)]'>O'qituvchining ish haqi ko'rsatkichi</h1>
-                            </div>
-
-                            <div className='w-[248px] h-[67px] bg-[rgba(242,238,238,1)] '>
-
-                            </div>
-                        </div>
-
-                        <div className='w-full h-[148px] gap-[20px] border-b-[1.5px] flex flex-col px-[10px]'>
-                            <div className='w-[677px] mt-[20px]'>
-                                <h1 className='text-[18px] font-[500] font-[inter] text-[rgba(64,64,64,1)]'>Qarzdorlik limiti</h1>
-                            </div>
-
-                            <div className='w-[248px] h-[67px] bg-[rgba(242,238,238,1)] '>
-
-                            </div>
-                        </div>
-
-                        
-
-
-
-
-
-
-
-                        <div className='w-full h-[62.17px] border-b-[1.5px] flex items-center px-[10px]'>
-                            <div className='w-[677px]'>
-                                <h1 className='font-[roboto] text-[18px] leading-[21.78px] text-[rgba(64,64,64,1)]'>Band xona</h1>
-                            </div>
-                            <img src={on} alt="" />
-                        </div>
-
                     </div>
                 </div>
             </div>
