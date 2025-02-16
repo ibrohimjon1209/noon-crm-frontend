6c9884e606ae217a41a01de4aaeea543605108f5
ibrohimjon1209 <ibrohimjonmamirov120909@gmail.com>
2025-02-16 14:56:00 +0500

    sfd23sdfad

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  server: {
+    allowedHosts: [
+      '9549-202-79-188-194.ngrok-free.app'
+    ]
+  }
 })
+
