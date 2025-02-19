83bfd70523432a684ced3037f5f39d4bf563dc61
Ilkhomov-web <ilhomovelyor119@gmail.com>
2025-02-19 22:46:45 +0500

    some change
    some change

--- src/pages/Students/Students.jsx
+++ src/pages/Students/Students.jsx
@@ -3,6 +3,7 @@ import New_students from "./childs/new_students/New_students";
 import { Route, Routes } from "react-router-dom";
 import Active_students from "./childs/active_students/Active_students";
 import Archive_students from "./childs/archive_student/Archive_students";
+import Students_list from "./childs/students_list/Students_list";
 
 const Teachers = () => {
   return (
@@ -10,6 +11,7 @@ const Teachers = () => {
       <Route path="new-student-list" element={<New_students />} />
       <Route path="students-active" element={<Active_students />} />
       <Route path="students-archive" element={<Archive_students />} />
+      <Route path="students-list" element={<Students_list />} />
     </Routes>
   );
 };
