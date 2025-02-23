859ec71d60d20ff0890397b5357988837089f383
Muhammadqodir <muhammadqodir@Macbook-Pro---valijonov.local>
2025-02-23 15:23:20 +0500

    UI: finance routes updated

--- src/pages/Finance/Finance.jsx
+++ src/pages/Finance/Finance.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Balance from './childs/balance/Balance'
+import Bonus from './childs/bonus/Bonus'
+import CashRegister from './childs/cashRegister/CashRegister'
+import Contract from './childs/contract/Contract'
+import GroupTransaction from './childs/groupTransaction/GroupTransaction'
+import IncomePlan from './childs/incomePlan/IncomePlan'
+import MonthlyPayment from './childs/monthlyPayment/MonthlyPayment'
+import Penalty from './childs/penalty/Penalty'
+import TypeTransaction from './childs/typeTransaction/TypeTransaction'
+import UnpaidPupil from './childs/unpaidPupil/UnpaidPupil'
 
 const Finance = () => {
   return (
-    <div></div>
+    <Routes>
+      <Route path='balance' element={<Balance />} />
+      <Route path='bonus' element={<Bonus />} />
+      <Route path='cash-register' element={<CashRegister />} />
+      <Route path='contract' element={<Contract />} />
+      <Route path='group-transaction' element={<GroupTransaction />} />
+      <Route path='income-plan' element={<IncomePlan />} />
+      <Route path='monthly-payment' element={<MonthlyPayment />} />
+      <Route path='penalty' element={<Penalty />} />
+      <Route path='type-transaction' element={<TypeTransaction />} />
+      <Route path='unpaid-pupil' element={<UnpaidPupil />} />
+    </Routes>
   )
 }
 
