bb704aa5022a09a537cfbf8519c5d88e73ea549b
Muhammadqodir <muhammadqodir@Macbook-Pro---valijonov.local>
2025-02-24 23:09:24 +0500

    UI: Payment updated v1

---
+++ src/pages/Finance/Action/Payment/SalaryActions.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SalaryActions = () => {
+  return (
+    <div>SalaryActions</div>
+  )
+}
+
+export default SalaryActions
---
+++ src/pages/Finance/Action/Payment/SalaryRow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SalaryRow = () => {
+  return (
+    <div>SalaryRow</div>
+  )
+}
+
+export default SalaryRow
