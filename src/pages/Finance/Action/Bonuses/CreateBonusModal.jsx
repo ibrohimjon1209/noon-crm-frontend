db724d44e4087720838a70c4ddc9a6e40a9cd48d
Muhammadqodir <muhammadqodir@Macbook-Pro---valijonov.local>
2025-02-24 14:56:20 +0500

    UI: Bonus finished

--- src/pages/Finance/Action/Bonuses/CreateBonusModal.jsx
+++ src/pages/Finance/Action/Bonuses/CreateBonusModal.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
+import { Drawer, Button, Input } from "antd";
 
 const CreateBonusModal = ({ onClose, addBonus, isModalOpen }) => {
   const [bonus, setBonus] = useState({
@@ -25,26 +23,28 @@ const CreateBonusModal = ({ onClose, addBonus, isModalOpen }) => {
   };
 
   return (
-    <Drawer
-      anchor="right"
-      open={isModalOpen}
-      onClose={onClose}
-    >
-      <Box
-        sx={{ width: 400 }}
-        role="presentation"
-      >
-        <form onSubmit={handleSubmit} style={{ padding: 20 }}>
-          <input type="text" name="type" placeholder="Bonus turi" onChange={handleChange} required />
-          <input type="text" name="fullName" placeholder="To'liq ismi" onChange={handleChange} required />
-          <input type="number" name="previousAmount" placeholder="Oldingi miqdor" onChange={handleChange} required />
-          <input type="number" name="amount" placeholder="Miqdori" onChange={handleChange} required />
-          <input type="number" name="nextAmount" placeholder="Keyingi miqdor" onChange={handleChange} required />
-          <input type="text" name="note" placeholder="Izoh" onChange={handleChange} />
-          <Button type="submit" variant="contained">Saqlash</Button>
-          <Button onClick={onClose}>Bekor qilish</Button>
-        </form>
-      </Box>
+    <Drawer 
+    title="Bonus yaratish" 
+    placement="right" 
+    onClose={onClose} 
+    open={isModalOpen}>
+      <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+        <Input name="type" placeholder="Bonus turi" onChange={handleChange} required />
+        <Input name="fullName" placeholder="To'liq ismi" onChange={handleChange} required />
+        <Input name="previousAmount" type="number" placeholder="Oldingi miqdor" onChange={handleChange} required />
+        <Input name="amount" type="number" placeholder="Miqdori" onChange={handleChange} required />
+        <Input name="nextAmount" type="number" placeholder="Keyingi miqdor" onChange={handleChange} required />
+        <Input name="note" placeholder="Izoh" onChange={handleChange} />
+
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Button onClick={onClose} type="default">
+            Orqaga
+          </Button>
+          <Button type="primary" htmlType="submit">
+            Saqlash
+          </Button>
+        </div>
+      </form>
     </Drawer>
   );
 };
