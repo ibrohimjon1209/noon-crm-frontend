f53fb6dec37d2a2302179bc8a447fdb2401684e7
yusuf-coder1 <yusufbekvalijonov0223@gmail.com>
2025-02-27 19:19:46 +0500

    fwefwe

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,14 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: "/",
   server: {
-<<<<<<< HEAD
+
     allowedHosts: [
       '2fd7-202-79-186-222.ngrok-free.app'
     ]
-  }
-  
-=======
-    allowedHosts: ['2fd7-202-79-186-222.ngrok-free.app']
   },
->>>>>>> 7264d81a4ca739d5a18757cd8157c5809447edc2
-})
+  
+
+    allowedHosts: ['2fd7-202-79-186-222.ngrok-free.app']
+},
+
+)
