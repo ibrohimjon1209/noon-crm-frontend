a6ac17a393c1be05de439a752d2ccfe49efb80ed
Muhammadqodir <muhammadqodir@Macbook-Pro---valijonov.local>
2025-02-27 20:53:08 +0500

    UI: added changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths"
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: "/",
   server: {
     allowedHosts: ['2fd7-202-79-186-222.ngrok-free.app']
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 })
