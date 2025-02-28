162738b4d6539b82f9c210c70ebfdf6438ba6280
Muhammadqodir <muhammadqodir@Macbook-Pro---valijonov.local>
2025-02-28 17:01:46 +0500

    UI: added changes v2

--- src/pages/Finance/Info/InputOutput/FIlterSection.jsx
+++ src/pages/Finance/Info/InputOutput/FIlterSection.jsx
@@ -1,22 +1,20 @@
 import { useState } from "react";
-import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
+import Select, { selectClasses } from "@mui/joy/Select";
+import Option from "@mui/joy/Option";
+import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
 
 const FilterSection = () => {
   const [activeTab, setActiveTab] = useState("Kirim");
-  const [kassa, setKassa] = useState("");
-  const [plastik, setPlastik] = useState("");
+  // const [kassa, setKassa] = useState("");
+  // const [plastik, setPlastik] = useState("");
 
-  const handleKassaChange = (event) => {
-    setKassa(event.target.value);
-  };
+  // const handleKassaChange = (event) => {
+  //   setKassa(event.target.value);
+  // };
 
-  const handlePlastikChange = (event) => {
-    setPlastik(event.target.value);
-  };
+  // const handlePlastikChange = (event) => {
+  //   setPlastik(event.target.value);
+  // };
 
   const tabs = ["Kirim", "Chiqim", "Vaucher"];
 
@@ -27,69 +25,72 @@ const FilterSection = () => {
           <button
             key={tab}
             onClick={() => setActiveTab(tab)}
-            className={`px-6 py-2 rounded-xl transition-colors duration-200 ${
-              activeTab === tab
-                ? "bg-blue-600 text-white"
-                : "bg-white text-black"
-            }`}
+            className={`px-6 py-2 rounded-xl transition-colors duration-200 ${activeTab === tab
+              ? "bg-blue-600 text-white"
+              : "bg-white text-black"
+              }`}
           >
             {tab}
           </button>
         ))}
       </div>
-      <button className="px-4 py-2 bg-blue-600 text-white rounded">
-        Export
-      </button>
+
       <div className="flex items-center justify-center gap-4 p-4 bg-white shadow-md rounded-md">
-        <FormControl sx={{ minWidth: 300 }}>
-          <InputLabel>Kassa</InputLabel>
-          <Select
-            value={kassa}
-            label="Kassa"
-            onChange={handleKassaChange}
-            MenuProps={{
-              PaperProps: {
-                style: {
-                  maxHeight: 300,
-                },
+        <button className="px-4 py-2 bg-blue-600 text-white rounded">
+          Export
+        </button>
+        <Select
+          placeholder="Select a pet…"
+          indicator={<KeyboardArrowDown />}
+          sx={{
+            width: 240,
+            [`& .${selectClasses.indicator}`]: {
+              transition: "0.2s",
+              [`&.${selectClasses.expanded}`]: {
+                transform: "rotate(-180deg)",
               },
-              anchorOrigin: {
-                vertical: "bottom",
-                horizontal: "left",
-              },
-              transformOrigin: {
-                vertical: "top",
-                horizontal: "left",
-              },
-            }}
-          >
-            <MenuItem value="kassa1">Kassa 1</MenuItem>
-            <MenuItem value="kassa2">Kassa 2</MenuItem>
-            <MenuItem value="kassa3">Kassa 3</MenuItem>
-          </Select>
-        </FormControl>
+            },
+          }}
+        >
+          <Option value="dog">Dog</Option>
+          <Option value="cat">Cat</Option>
+          <Option value="fish">Fish</Option>
+          <Option value="bird">Bird</Option>
+        </Select>
 
         <input
           style={{ minWidth: 300 }}
           type="date"
           className="px-4 py-2 border rounded bg-gray-100"
         />
+        <Select
+          placeholder="Select a pet…"
+          indicator={<KeyboardArrowDown />}
+          disablePortal // <-- Bu select ochilganda joyida qolishini ta'minlaydi
+          sx={{
+            width: 240,
+            [`& .${selectClasses.indicator}`]: {
+              transition: "0.2s",
+              [`&.${selectClasses.expanded}`]: {
+                transform: "rotate(-180deg)",
+              },
+            },
+          }}
+          slotProps={{
+            listbox: {
+              sx: {
+                position: "absolute", // <-- Bu select opsiyalarini o'z joyida ochilishiga yordam beradi
+                zIndex: 1000, // <-- Element oldinga chiqib turishi uchun
+              },
+            },
+          }}
+        >
+          <Option value="dog">Dog</Option>
+          <Option value="cat">Cat</Option>
+          <Option value="fish">Fish</Option>
+          <Option value="bird">Bird</Option>
+        </Select>
 
-        <FormControl sx={{ minWidth: 300 }}>
-          <InputLabel>To'lov turi</InputLabel>
-          <Select
-            value={plastik}
-            label="Plastik"
-            onChange={handlePlastikChange}
-          >
-            <MenuItem value="plastik1">Naqd</MenuItem>
-            <MenuItem value="plastik2">Plastik</MenuItem>
-            <MenuItem value="plastik3">Terminal</MenuItem>
-            <MenuItem value="plastik4">Payme</MenuItem>
-            <MenuItem value="plastik5">Click</MenuItem>
-            <MenuItem value="plastik6">Uzum</MenuItem>
-          </Select>
-        </FormControl>
       </div>
     </div>
   );
