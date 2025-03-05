2200d7dd7d69f9476a383d752465487bf3375667
Muhammadqodir <muhammadqodir@Macbook-Pro---valijonov.local>
2025-03-05 20:29:29 +0500

    UI: contract updated v3

--- src/pages/Finance/Report/Contract/ContractTable.jsx
+++ src/pages/Finance/Report/Contract/ContractTable.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
-
-const ContractTable = () => {
+import PropTypes from 'prop-types';
+import TableRow from './TableRow';
+const ContractTable = ({ data = [] }) => {
   return (
-    <div>ContractTable</div>
-  )
-}
+    <div className="w-full p-4 bg-white shadow-md rounded-md m-8  overflow-auto">
+      <table className="w-full h-full table-auto">
+        <thead className='bg-gray-50'>
+          <tr className="bg-gray-50 border-b border-gray-200">
+            <th className="px-6 py-3 text-left text-base font-medium text-gray-500 uppercase tracking-wider">№</th>
+            <th className="px-6 py-3 text-left text-lg font-medium text-gray-500 uppercase tracking-wider">o'quvchi</th>
+            <th className="px-6 py-3 text-left text-lg font-medium text-gray-500 uppercase tracking-wider">Balans</th>
+            <th className="px-6 py-3 text-left text-lg font-medium text-gray-500 uppercase tracking-wider">Moderator	</th>
+            <th className="px-6 py-3 text-left text-lg font-medium text-gray-500 uppercase tracking-wider">Miqdori</th>
+            <th className="px-6 py-3 text-left text-lg font-medium text-gray-500 uppercase tracking-wider">Kutilayotgan to'lov miqdori</th>
+            <th className="px-6 py-3 text-left text-lg font-medium text-gray-500 uppercase tracking-wider">To'langan miqdor</th>
+            <th className="px-6 py-3 text-left text-lg font-medium text-gray-500 uppercase tracking-wider">Yaratilgan sanasi</th>
+            <th className="px-6 py-3 text-left text-lg font-medium text-gray-500 uppercase tracking-wider">Izoh</th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {data.length > 0 ? (
+            data.map((item) => (
+              <TableRow
+                key={item.id || crypto.randomUUID()}
+                item={item}
+              />
+            ))
+          ) : (
+            <tr>
+              <td
+                colSpan="11"
+                className="px-6 py-16 text-center text-sm text-gray-600 font-semibold"
+              >
+                Ma'lumotlar topilmadi
+              </td>
+            </tr>
+          )}
+        </tbody>
 
-export default ContractTable
+      </table>
+    </div>
+  );
+};
+
+ContractTable.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      oylik: PropTypes.number,
+      davomat: PropTypes.number,
+      davomatFoizi: PropTypes.number,
+      bonus: PropTypes.number,
+      avans: PropTypes.number,
+      jarima: PropTypes.number,
+      akladi: PropTypes.number,
+      tolanmagan: PropTypes.number,
+      sana: PropTypes.string,
+      tolanmagan: PropTypes.number
+    })
+  ).isRequired
+};
+
+
+export default ContractTable;
