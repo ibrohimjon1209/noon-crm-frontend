a9e03ca2106edac5ed228125c7508811a447033c
Muhammadqodir <muhammadqodir@Macbook-Pro---valijonov.local>
2025-03-05 16:36:28 +0500

    UI:Contract updated v1

---
+++ src/pages/Finance/Report/Contract/ContractDrawer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContractDrawer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ContractDrawer
---
+++ src/pages/Finance/Report/Contract/ContractFilter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContractFilter = () => {
+  return (
+    <div>
+       
+    </div>
+  )
+}
+
+export default ContractFilter
