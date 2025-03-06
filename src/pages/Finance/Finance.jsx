83b95c07776696b65cec2b1f44c8b76f7a3168e5
Ilkhomov-web <ilhomovelyor119@gmail.com>
2025-03-06 23:58:54 +0300

    file path tugrladim
    file path tugrladim

--- src/pages/Finance/Finance.jsx
+++ src/pages/Finance/Finance.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Bonus from './Action/Bonuses/Bonus'
-import Penalty from './Action/Penalties/Penalty'
-import Monthly_payment from './Action/Payment/MonthlyPayment'
-import Input_output from './Info/InputOutput/Input_output'
-import Revenue from './Info/revenues/Revenue'
-import Transaction_wihtout_bills from './Info/Transaction_wihtout_bills'
-import Transaction from './Report/Transaction'
-import Contract from './Report/Contract'
-import Balance from './Info/Balans/Balance'
-import Cassa from './Action/Cassa/Cassa'
-import Unpaid_bills from './info/Unpaid/Unpaid_bills_main'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Bonus from "./Action/Bonuses/Bonus";
+import Penalty from "./Action/Penalties/Penalty";
+import Monthly_payment from "./Action/Payment/MonthlyPayment";
+import Input_output from "./Info/InputOutput/Input_output";
+import Revenue from "./Info/revenues/Revenue";
+import Transaction_wihtout_bills from "./Info/TransactionBills/Transaction_without_bills";
+import Transaction from "./Report/Transactions/Transaction";
+import Contract from "./Report/Contract/Contract";
+import Balance from "./Info/Balans/Balance";
+import Cassa from "./Action/Cassa/Cassa";
+import Unpaid_bills from "./info/Unpaid/Unpaid_bills_main";
 
 const Finance = () => {
   return (
@@ -25,7 +25,7 @@ const Finance = () => {
       <Route path="/unpaid-bills" element={<Unpaid_bills />} />
       <Route
         path="/transaction-without-bills"
-        element={<Transaction_without_bills />}
+        element={<Transaction_wihtout_bills />}
       />
       <Route path="/transaction" element={<Transaction />} />
       <Route path="/contract" element={<Contract />} />
