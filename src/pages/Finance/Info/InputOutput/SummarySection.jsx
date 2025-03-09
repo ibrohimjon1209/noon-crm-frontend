9c106606dc5f1ea0d8672b5b34488e0cbe630a2f
Muhammadqodir <muhammadqodir@Macbook-Pro---valijonov.local>
2025-03-10 02:36:03 +0500

    UI:file download added in Input Output

--- src/pages/Finance/Info/InputOutput/SummarySection.jsx
+++ src/pages/Finance/Info/InputOutput/SummarySection.jsx
@@ -1,8 +1,10 @@
-const SummarySection = () => {
+const SummarySection = ({data}) => {
+  const totalSum = data.reduce((sum, item) => sum + item.amount, 0);
+
     return (
       <div className="p-4 bg-white shadow-md rounded-md mt-4 flex justify-between">
         <p>Jami</p>
-        <p>0 UZS</p>
+        <p>{totalSum.toLocaleString()} so‘m</p>
       </div>
     );
   };
