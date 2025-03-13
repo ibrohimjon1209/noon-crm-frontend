925dd0c11f511c31fa35a8e04899a2fa3b14c87d
Muhammadqodir <muhammadqodir@Macbook-Pro---valijonov.local>
2025-03-13 22:59:05 +0500

    UI: made changes tabs

--- src/pages/Finance/Action/Cassa/TransactionTable/TransactionTabs.jsx
+++ src/pages/Finance/Action/Cassa/TransactionTable/TransactionTabs.jsx
@@ -5,12 +5,12 @@ const TransactionTabs = () => {
   const tabs = ["Tranzaksiya", "Ilova Orqali To'lov"];
   
   return (
-    <div className="flex bg-gray-100 p-1 rounded-lg mt-4 ml-4">
+    <div className="flex p-1 rounded-lg mt-4 ml-4 mb-6 ">
       {tabs.map((tab) => (
         <button
           key={tab}
           onClick={() => setActiveTab(tab)}
-          className={`px-6 py-2 rounded-xl transition-colors duration-200 ${
+          className={`px-6 py-2 rounded-sm transition-colors duration-200  ${
             activeTab === tab ? "bg-blue-600 text-white" : "bg-white text-black"
           }`}
         >
